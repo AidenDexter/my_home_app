bdb2a1e0eb07799413cc13be5dcb03b842a64c72
slava-vl <slavabolotaev@gmail.com>
2024-08-09 17:56:54 +0300

    Added theme_extension

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'core/environment/app_environment.dart';
 
 import 'runner/runner.dart' as runner;
 
+import 'core/theme/app_theme.dart';
+
 void main() {
   AppEnvironment.init(
     config: const AppConfig(
