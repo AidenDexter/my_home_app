945889ca62e64bdf86d26134479e4d06a1880220
Aiden <97030039+AidenDexter@users.noreply.github.com>
2024-08-15 22:10:35 +0400

    refactor: decorated_container

--- lib/core/ui_kit/decorated_container.dart
+++ lib/core/ui_kit/decorated_container.dart
@@ -4,25 +4,23 @@ import '../extension/extensions.dart';
 
 class DecoratedContainer extends StatelessWidget {
   final Widget child;
-  final EdgeInsets? padding;
+  final EdgeInsets padding;
   const DecoratedContainer({
     required this.child,
-    this.padding,
+    this.padding = const EdgeInsets.all(16),
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: BoxDecoration(
-        color: context.theme.commonColors.white,
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(color: context.theme.commonColors.neutralgrey10),
-      ),
-      child: Padding(
-        padding: padding ?? const EdgeInsets.all(16),
-        child: child,
-      ),
-    );
-  }
+  Widget build(BuildContext context) => DecoratedBox(
+        decoration: BoxDecoration(
+          color: context.theme.commonColors.white,
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          border: Border.all(color: context.theme.commonColors.neutralgrey10),
+        ),
+        child: Padding(
+          padding: padding,
+          child: child,
+        ),
+      );
 }
