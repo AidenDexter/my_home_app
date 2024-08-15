0c94997bf97ba9d7ae70d50872d683b1ec14a883
Aiden <97030039+AidenDexter@users.noreply.github.com>
2024-08-15 21:26:10 +0400

    feat: added circle_button and vip_card

---
+++ lib/core/ui_kit/circle_button.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+import '../extension/extensions.dart';
+
+class CircleButton extends StatelessWidget {
+  final Widget icon;
+  final Color? backgroundColor;
+  final double dimension;
+  final VoidCallback onTap;
+  const CircleButton({
+    required this.icon,
+    required this.onTap,
+    super.key,
+    this.backgroundColor,
+    this.dimension = 36,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.square(
+      dimension: dimension,
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(dimension / 2),
+        child: Ink(
+          decoration: BoxDecoration(
+            color: backgroundColor ?? context.theme.commonColors.white,
+            borderRadius: BorderRadius.circular(dimension / 2),
+          ),
+          child: Center(child: icon),
+        ),
+      ),
+    );
+  }
+}
--- lib/core/ui_kit/vip_card.dart
+++ lib/core/ui_kit/vip_card.dart
@@ -1,40 +1,13 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
-import '../extension/src/theme_extension.dart';
+import '../resources/assets.gen.dart';
 
 class VipCard extends StatelessWidget {
-  final Color backgroundColor;
-  final String cardTitle;
-
-  const VipCard.vip({super.key})
-      : cardTitle = 'VIP',
-        backgroundColor = const Color(0xFF3B73FC);
-  const VipCard.superVip({super.key})
-      : cardTitle = 'S-VIP',
-        backgroundColor = const Color(0xFFFF3D00);
-  const VipCard.vipPlus({super.key})
-      : cardTitle = 'VIP+',
-        backgroundColor = const Color(0xFFFABD00);
+  final SvgGenImage vipType;
+  VipCard.vip({super.key}) : vipType = Assets.icons.vip;
+  VipCard.superVip({super.key}) : vipType = Assets.icons.vipSuper;
+  VipCard.vipPlus({super.key}) : vipType = Assets.icons.vipPlus;
 
   @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 22,
-      width: 36,
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          color: backgroundColor,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Center(
-          child: Text(
-            cardTitle,
-            style: context.theme.commonTextStyles.body2
-                .copyWith(color: context.theme.commonColors.white),
-          ),
-        ),
-      ),
-    );
-  }
+  Widget build(BuildContext context) => vipType.svg();
 }
