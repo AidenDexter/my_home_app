188e31f21af9d30ec6f28fbdda5f50ca8f5ebadc
sl_vl <slavabolotaev@gmail.com>
2024-08-17 09:29:25 +0400

    Added multi & single selection cards

--- lib/feature/mock/presentation/mock_page.dart
+++ lib/feature/mock/presentation/mock_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../core/extension/src/error_extension.dart';
+import '../../../core/ui_kit/multi_selection_card.dart';
 import '../../../core/ui_kit/primary_bottom_sheet.dart';
+import '../../../core/ui_kit/single_selection_card.dart';
 import '../../../core/ui_kit/vip_card.dart';
 import '../bloc/mock_bloc.dart';
 import '../domain/entity/post.dart';
@@ -32,20 +34,106 @@ class MockPage extends StatelessWidget {
   }
 }
 
-class _DataLayout extends StatelessWidget {
+class _DataLayout extends StatefulWidget {
   final Post post;
   const _DataLayout({required this.post});
 
   @override
-  Widget build(BuildContext context) => InkWell(
-        child: const VipCard.vip(),
-        onTap: () => PrimaryBottomSheet.show(
-          context: context,
-          builder: (context) => const Column(
-            children: [Text('text'), Text('text')],
-          ),
+  State<_DataLayout> createState() => _DataLayoutState();
+}
+
+class _DataLayoutState extends State<_DataLayout> {
+  late final ValueNotifier<int?> _singleSelectionGroupValue;
+  late final ValueNotifier<List<int>> _multiSelectionValue;
+
+  @override
+  void initState() {
+    _singleSelectionGroupValue = ValueNotifier(null);
+    _multiSelectionValue = ValueNotifier([]);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) => Center(
+        child: Column(
+          children: [
+            InkWell(
+              child: VipCard.vip(),
+              onTap: () => PrimaryBottomSheet.show(
+                context: context,
+                builder: (context) => const Column(
+                  children: [Text('text'), Text('text')],
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            AnimatedBuilder(
+              animation: _singleSelectionGroupValue,
+              builder: (context, child) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SingleSelectionCard(
+                      onTap: (value) => _singleSelectionGroupValue.value = value,
+                      groupValue: _singleSelectionGroupValue.value,
+                      value: 1,
+                      title: 'Аренда',
+                    ),
+                    SingleSelectionCard(
+                      onTap: (value) => _singleSelectionGroupValue.value = value,
+                      groupValue: _singleSelectionGroupValue.value,
+                      value: 2,
+                      title: 'Продажа',
+                    ),
+                  ],
+                );
+              },
+            ),
+            const SizedBox(height: 16),
+            AnimatedBuilder(
+              animation: _multiSelectionValue,
+              builder: (context, child) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    MultiSelectionCard(
+                      onTap: (value) {
+                        if (_multiSelectionValue.value.contains(1)) {
+                          _multiSelectionValue.value = List.from(_multiSelectionValue.value)..remove(value);
+                          return;
+                        }
+                        _multiSelectionValue.value = List.from(_multiSelectionValue.value)..add(value);
+                      },
+                      isSelected: _multiSelectionValue.value.contains(1),
+                      value: 1,
+                      title: 'Аренда',
+                    ),
+                    MultiSelectionCard(
+                      onTap: (value) {
+                        if (_multiSelectionValue.value.contains(2)) {
+                          _multiSelectionValue.value = List.from(_multiSelectionValue.value)..remove(value);
+                          return;
+                        }
+                        _multiSelectionValue.value = List.from(_multiSelectionValue.value)..add(value);
+                      },
+                      isSelected: _multiSelectionValue.value.contains(2),
+                      value: 2,
+                      title: 'Продажа',
+                    ),
+                  ],
+                );
+              },
+            ),
+          ],
         ),
       );
+
+  @override
+  void dispose() {
+    _singleSelectionGroupValue.dispose();
+    _multiSelectionValue.dispose();
+    super.dispose();
+  }
 }
 
 class _ErrorLayout extends StatelessWidget {
